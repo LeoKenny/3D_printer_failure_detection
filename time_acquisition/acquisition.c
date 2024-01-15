25acbc66b1c73f63c5930a2c73da1638a243fdc9
LeoKenny <leo.treicheldacunha@gmail.com>
2024-01-15 02:18:56 -0300

    feat: watermark interrupt and buffer allocation working

--- time_acquisition/acquisition.c
+++ time_acquisition/acquisition.c
@@ -18,7 +18,10 @@
 #define FIFO_STATUS 0x39    // FIFO status register address
 #define WATERMARK_SIZE 16   // Watermark sample size
 
-const double conversion_const = (2.0*16.0)/8192;       // +-16g for 13 bits, pg 27
+#define WATERMARK_PIN 27    // Watermark interrupt pin
+// #define WATERMARK_PIN 7    // Watermark interrupt pin
+
+const double conversion_const = (2.0*16.0)/8192;    // +-16g for 13 bits, pg 27
 const int rw_bit = 7;                               // Read/Write bit
 const int multi_byte_bit = 6;                       // Mutiple byte bit
 const int fifo_size = 33;                           // Max number of saved values, FIFO + last reading
@@ -32,6 +35,7 @@ const double delay_read = 0.006;                    // Delay for reading FIFO up
 // Buffer handling
 pthread_mutex_t bufferAMutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t bufferBMutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t bufferCMutex = PTHREAD_MUTEX_INITIALIZER;
 
 typedef struct data_buffer {
     int samples_counter;
@@ -46,13 +50,13 @@ typedef struct data_buffer {
 
 typedef struct buffer_handler{
     struct timespec start_time;
-    int overrun_trigger;
     data_buffer bufferA;
-    // data_buffer bufferB;
-    unsigned long int sample_number_A;    // Samples total number to sort order
-    // unsigned long int sample_number_B;    // Samples total number to sort order
-    // int racing;         // Indicates the racing condition happened
-    int updated;        // Indicates a new reading have been done
+    data_buffer bufferB;
+    data_buffer bufferC;
+    unsigned long int sample_number;    // Samples total number to sort order
+    int updated_A;        // Indicates a new reading have been done in buffer A
+    int updated_B;        // Indicates a new reading have been done in buffer B
+    int updated_C;        // Indicates a new reading have been done in buffer C
     int spi_handle;
 } buffer_handler;
 
@@ -169,10 +173,10 @@ void configure_adxl(int spi_handle){
 
     // Set interrupt pin map
     command[0] = INT_MAP;
-    command[1] = 0x02;
+    command[1] = 0x01;
     // DATA_READY | SINGLE_TAP | DOUBLE_TAP | Activity | Inactivity | FREE_FALL | Watermark | Overrun
-    //      0     |      0     |      0     |     0    |      0     |     0     |     1     |    0
-    //    INT1    |    INT1    |    INT1    |   INT1   |    INT1    |   INT1    |    INT2   |   INT1
+    //      0     |      0     |      0     |     0    |      0     |     0     |     0     |    1
+    //    INT1    |    INT1    |    INT1    |   INT1   |    INT1    |   INT1    |    INT1   |   INT2
     // (datasheet ADXL345, pg 26)
     spi_write(spi_handle, command, 2);
     delay_ms(1);
@@ -253,54 +257,124 @@ void save_data(char *output_name, data_buffer buffer){
                 buffer.x_data[i], buffer.y_data[i], buffer.z_data[i]);
     }
     fclose(pFile);
-    buffer.samples_counter = 0;
 }
 
-// void watermarkInterruptHandler(int gpio, int level, uint32_t tick, void * exData) {
-void watermarkInterruptHandler(void * exData) {
+void watermarkInterruptHandler(int gpio, int level, uint32_t tick, void * exData) {
+// void watermarkInterruptHandler(void * exData) {
     char spi_buffer[spi_buffer_size];
     char command[spi_buffer_size];
     int16_t x, y, z;
     int result;
-    double t_sample;
-    double t_block;
+    double t_sample,t_block;
+    int selected;                   // Selected buffer
 
     buffer_handler *handler = (buffer_handler *)exData;
-
     t_block = time_delta_now(handler->start_time);
-    pthread_mutex_lock(&bufferAMutex);
 
-    for(int i=0; i<WATERMARK_SIZE; i++){
-        command[0] = DATAX0;
-        result = spi_read(handler->spi_handle, command, spi_buffer, spi_buffer_size);
-        if(result < spi_buffer_size){
-            printf("Buffer read size {%d} is smaller than expected {%d}.\n", result, spi_buffer_size);
-        }
-        else{
-            t_sample = time_delta_now(handler->start_time);
-            x = (spi_buffer[2]<<8) | spi_buffer[1];
-            y = (spi_buffer[4]<<8) | spi_buffer[3];
-            z = (spi_buffer[6]<<8) | spi_buffer[5];
-
-            handler->bufferA.number_data[handler->bufferA.samples_counter] = handler->sample_number_A;
-            handler->bufferA.block_time_data[handler->bufferA.samples_counter] = t_block;
-            handler->bufferA.sample_time_data[handler->bufferA.samples_counter] = t_sample;
-            handler->bufferA.overrun_data[handler->bufferA.samples_counter] = handler->overrun_trigger;
-            handler->bufferA.x_data[handler->bufferA.samples_counter] = (double)x * conversion_const;
-            handler->bufferA.y_data[handler->bufferA.samples_counter] = (double)y * conversion_const;
-            handler->bufferA.z_data[handler->bufferA.samples_counter] = (double)z * conversion_const;
-
-            handler->bufferA.samples_counter++;
-            handler->sample_number_A++;
-        }
-        delay_ms(0.005);
+    if(handler->updated_A == 0){
+        if(pthread_mutex_trylock(&bufferAMutex) == 0)   selected = 1;
+        else printf("\nFailed to open buffer A\n");
     }
-    handler->updated = 1;
-    pthread_mutex_unlock(&bufferAMutex);
+    else if(handler->updated_B == 0){
+        if(pthread_mutex_trylock(&bufferBMutex) == 0)   selected = 2;
+        else printf("\nFailed to open buffer B\n");
+    }
+    else if(handler->updated_C == 0){
+        if(pthread_mutex_trylock(&bufferCMutex) == 0)   selected = 3;
+        else printf("\nFailed to open buffer C\n");
+    }
+    else return;
+
+    if (selected == 1){
+        for(int i=0; i<WATERMARK_SIZE; i++){
+            command[0] = DATAX0;
+            result = spi_read(handler->spi_handle, command, spi_buffer, spi_buffer_size);
+            if(result < spi_buffer_size){
+                printf("Buffer read size {%d} is smaller than expected {%d}.\n", result, spi_buffer_size);
+            }
+            else{
+                t_sample = time_delta_now(handler->start_time);
+                x = (spi_buffer[2]<<8) | spi_buffer[1];
+                y = (spi_buffer[4]<<8) | spi_buffer[3];
+                z = (spi_buffer[6]<<8) | spi_buffer[5];
+
+                handler->bufferA.number_data[handler->bufferA.samples_counter] = handler->sample_number;
+                handler->bufferA.block_time_data[handler->bufferA.samples_counter] = t_block;
+                handler->bufferA.sample_time_data[handler->bufferA.samples_counter] = t_sample;
+                handler->bufferA.overrun_data[handler->bufferA.samples_counter] = 0;
+                handler->bufferA.x_data[handler->bufferA.samples_counter] = (double)x * conversion_const;
+                handler->bufferA.y_data[handler->bufferA.samples_counter] = (double)y * conversion_const;
+                handler->bufferA.z_data[handler->bufferA.samples_counter] = (double)z * conversion_const;
+
+                handler->bufferA.samples_counter = handler->bufferA.samples_counter + 1;
+                handler->sample_number = handler->sample_number + 1;
+            }
+            delay_ms(0.005);
+        }
+        pthread_mutex_unlock(&bufferAMutex);
+        handler->updated_A = 1;
+    }
+    if (selected == 2){
+        for(int i=0; i<WATERMARK_SIZE; i++){
+            command[0] = DATAX0;
+            result = spi_read(handler->spi_handle, command, spi_buffer, spi_buffer_size);
+            if(result < spi_buffer_size){
+                printf("Buffer read size {%d} is smaller than expected {%d}.\n", result, spi_buffer_size);
+            }
+            else{
+                t_sample = time_delta_now(handler->start_time);
+                x = (spi_buffer[2]<<8) | spi_buffer[1];
+                y = (spi_buffer[4]<<8) | spi_buffer[3];
+                z = (spi_buffer[6]<<8) | spi_buffer[5];
+
+                handler->bufferB.number_data[handler->bufferB.samples_counter] = (handler->sample_number);
+                handler->bufferB.block_time_data[handler->bufferB.samples_counter] = t_block;
+                handler->bufferB.sample_time_data[handler->bufferB.samples_counter] = t_sample;
+                handler->bufferB.overrun_data[handler->bufferB.samples_counter] = 0;
+                handler->bufferB.x_data[handler->bufferB.samples_counter] = (double)x * conversion_const;
+                handler->bufferB.y_data[handler->bufferB.samples_counter] = (double)y * conversion_const;
+                handler->bufferB.z_data[handler->bufferB.samples_counter] = (double)z * conversion_const;
+
+                handler->bufferB.samples_counter = handler->bufferB.samples_counter + 1;
+                handler->sample_number = handler->sample_number + 1;
+            }
+            delay_ms(0.005);
+        }
+        pthread_mutex_unlock(&bufferBMutex);
+        handler->updated_B = 1;
+    }
+    if (selected == 3){
+        for(int i=0; i<WATERMARK_SIZE; i++){
+            command[0] = DATAX0;
+            result = spi_read(handler->spi_handle, command, spi_buffer, spi_buffer_size);
+            if(result < spi_buffer_size){
+                printf("Buffer read size {%d} is smaller than expected {%d}.\n", result, spi_buffer_size);
+            }
+            else{
+                t_sample = time_delta_now(handler->start_time);
+                x = (spi_buffer[2]<<8) | spi_buffer[1];
+                y = (spi_buffer[4]<<8) | spi_buffer[3];
+                z = (spi_buffer[6]<<8) | spi_buffer[5];
+
+                handler->bufferC.number_data[handler->bufferC.samples_counter] = (handler->sample_number);
+                handler->bufferC.block_time_data[handler->bufferC.samples_counter] = t_block;
+                handler->bufferC.sample_time_data[handler->bufferC.samples_counter] = t_sample;
+                handler->bufferC.overrun_data[handler->bufferC.samples_counter] = 0;
+                handler->bufferC.x_data[handler->bufferC.samples_counter] = (double)x * conversion_const;
+                handler->bufferC.y_data[handler->bufferC.samples_counter] = (double)y * conversion_const;
+                handler->bufferC.z_data[handler->bufferC.samples_counter] = (double)z * conversion_const;
+
+                handler->bufferC.samples_counter = handler->bufferC.samples_counter + 1;
+                handler->sample_number = handler->sample_number + 1;
+            }
+            delay_ms(0.005);
+        }
+        pthread_mutex_unlock(&bufferCMutex);
+        handler->updated_C = 1;
+    }
 }
 
 int main(int argc, char *argv[]) {
-    int watermark_trigger, overrun_trigger;
     buffer_handler handler;
     double sample_time = 5;              // sample time in seconds
     int sample_rate = 3200;                 // sample rate in Hz
@@ -317,7 +391,29 @@ int main(int argc, char *argv[]) {
         malloc(fifo_size * sizeof(double)),             // block_time_data
         malloc(fifo_size * sizeof(double)),             // sample_time_data
     };
+    data_buffer bufferB = {
+        0, // samples_counter
+        malloc(fifo_size * sizeof(unsigned long int)),  // number_data
+        malloc(fifo_size * sizeof(int)),                // overrun_data
+        malloc(fifo_size * sizeof(double)),             // x_data
+        malloc(fifo_size * sizeof(double)),             // y_data
+        malloc(fifo_size * sizeof(double)),             // z_data
+        malloc(fifo_size * sizeof(double)),             // block_time_data
+        malloc(fifo_size * sizeof(double)),             // sample_time_data
+    };
+    data_buffer bufferC = {
+        0, // samples_counter
+        malloc(fifo_size * sizeof(unsigned long int)),  // number_data
+        malloc(fifo_size * sizeof(int)),                // overrun_data
+        malloc(fifo_size * sizeof(double)),             // x_data
+        malloc(fifo_size * sizeof(double)),             // y_data
+        malloc(fifo_size * sizeof(double)),             // z_data
+        malloc(fifo_size * sizeof(double)),             // block_time_data
+        malloc(fifo_size * sizeof(double)),             // sample_time_data
+    };
     handler.bufferA = bufferA;
+    handler.bufferB = bufferB;
+    handler.bufferC = bufferC;
 
     // Create Output file, and put header
     FILE * pFile;
@@ -330,6 +426,10 @@ int main(int argc, char *argv[]) {
         printf("GPIO initialization failed.");
         return 1;
     }
+    // Starting mutex
+    pthread_mutex_init(&bufferAMutex, NULL);
+    pthread_mutex_init(&bufferBMutex, NULL);
+    pthread_mutex_init(&bufferCMutex, NULL);
 
     // Starting SPI
     handler.spi_handle = spiOpen(spi_channel, spi_speed, 3);
@@ -340,37 +440,63 @@ int main(int argc, char *argv[]) {
     // Starting acquisition
     printf("Sample Time: %.6f seconds\n",sample_time);
 
-    handler.updated = 0;
-    handler.sample_number_A = 0;
+    handler.updated_A = 0;
+    handler.updated_B = 0;
+    handler.updated_C = 0;
+    handler.sample_number = 0;
     clear_fifo_forced(handler.spi_handle);
-    delay_ms(1/(double)sample_rate);
+
+    // Set up the interrupt handler
+    if (gpioSetAlertFuncEx(WATERMARK_PIN, &watermarkInterruptHandler, &handler) < 0) {
+        fprintf(stderr, "Error setting up interrupt\n");
+        gpioTerminate();
+        return 1;
+    }
+    gpioSetPullUpDown(WATERMARK_PIN, PI_PUD_UP);
+
+    delay_ms(1/((double)sample_rate*2));
 
     clock_gettime(CLOCK_REALTIME, &(handler.start_time));
 
     while(time_delta_now(handler.start_time) < sample_time){
-        fifo_status(handler.spi_handle);
-        trigger_status(handler.spi_handle, &watermark_trigger, &overrun_trigger);
-        handler.overrun_trigger = overrun_trigger;
+        if(handler.updated_A){
+            if(pthread_mutex_trylock(&bufferAMutex) == 0){
+                save_data(output_name,handler.bufferA);
 
-        if(overrun_trigger){
-            printf("Overrun!");
-            printf("Delta: %f\n", time_delta_now(handler.start_time));
+                handler.updated_A = 0;
+                handler.bufferA.samples_counter = 0;
+
+                pthread_mutex_unlock(&bufferAMutex);
+            }
         }
 
-        if(watermark_trigger>0){
-            watermarkInterruptHandler(&handler);
+        if(handler.updated_B){
+            if(pthread_mutex_trylock(&bufferBMutex) == 0){
+            save_data(output_name,handler.bufferB);
+
+            handler.updated_B = 0;
+            handler.bufferB.samples_counter = 0;
+
+            pthread_mutex_unlock(&bufferBMutex);
+            }
         }
-        if(handler.updated == 1){
-            save_data(output_name,
-                      handler.bufferA);
-            handler.bufferA.samples_counter = 0;
-            handler.updated = 0;
+
+        if(handler.updated_C){
+            if(pthread_mutex_trylock(&bufferCMutex) == 0){
+                save_data(output_name,handler.bufferC);
+
+                handler.updated_C = 0;
+                handler.bufferC.samples_counter = 0;
+
+                pthread_mutex_unlock(&bufferCMutex);
+            }
         }
-        delay_ms(MS_PER_SECOND/((double)sample_rate*2));
+
+        delay_ms(MS_PER_SECOND/(double)sample_rate);
     }
 
     printf("\nElapsed Time: %.6f seconds\n", time_delta_now(handler.start_time));
-    printf("Samples Quantity: %ld\n", handler.sample_number_A);
+    printf("Samples Quantity: %ld\n", handler.sample_number);
 
     // Finishing SPI and GPIO
     spiClose(handler.spi_handle);
@@ -383,6 +509,26 @@ int main(int argc, char *argv[]) {
     free(bufferA.block_time_data);
     free(bufferA.sample_time_data);
     free(bufferA.overrun_data);
+
+    free(bufferB.x_data);
+    free(bufferB.y_data);
+    free(bufferB.z_data);
+    free(bufferB.number_data);
+    free(bufferB.block_time_data);
+    free(bufferB.sample_time_data);
+    free(bufferB.overrun_data);
+
+    free(bufferC.x_data);
+    free(bufferC.y_data);
+    free(bufferC.z_data);
+    free(bufferC.number_data);
+    free(bufferC.block_time_data);
+    free(bufferC.sample_time_data);
+    free(bufferC.overrun_data);
+
+    pthread_mutex_destroy(&bufferAMutex);
+    pthread_mutex_destroy(&bufferBMutex);
+    pthread_mutex_destroy(&bufferCMutex);
 
     printf("Done\n");
     return 0;
